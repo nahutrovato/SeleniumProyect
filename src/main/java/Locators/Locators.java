6026a892a84a12f4c142cba6295f6deb243f31a0
nahutrovato <trovatonahuel@gmail.com>
2022-05-17 17:15:58 -0300

    Login test

--- src/main/java/Locators/Locators.java
+++ src/main/java/Locators/Locators.java
@@ -1,4 +1,5 @@
 package Locators;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,6 +14,23 @@ public class Locators {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.saucedemo.com/");
+
+        //Credentials
+        String username = "standard_user";
+        String password = "secret_sauce";
+
+        //Locators by ID
+        WebElement inputUserName = driver.findElement(By.id("user-name"));
+        WebElement inputPassword = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        //Login
+        inputUserName.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginBtn.click();
+
+        //Into the page
+        WebElement title = driver.findElement(By.className("title"));
 
 
 
