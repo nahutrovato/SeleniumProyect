cf47e2b0bb73b451b2e4a624b96264418835b1de
nahutrovato <trovatonahuel@gmail.com>
2022-05-17 20:34:38 -0300

    Tests FindElements

--- src/main/java/Locators/FindElements.java
+++ src/main/java/Locators/FindElements.java
@@ -1,7 +1,11 @@
 package Locators;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FindElements {
@@ -13,6 +17,27 @@ public class FindElements {
         WebDriver driver = new ChromeDriver();
 
         driver.get(url);
+
+        //Credentials
+        String username = "standard_user";
+        String password = "secret_sauce";
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        List<String> credentials = new ArrayList<>();
+        credentials.add(username);
+        credentials.add(password);
+
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        System.out.println("Cantidad de elementos de tipo input: " +inputs.size());
+
+        for (int i=0 ; i<inputs.size(); i++) {
+            if(i == 2) {
+                loginBtn.click();
+            }
+            inputs.get(i).sendKeys(credentials.get(i));
+
+
+        }
 
     }
 }
