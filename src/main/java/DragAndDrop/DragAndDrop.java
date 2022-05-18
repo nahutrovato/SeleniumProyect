31d50438f51ba876e255ea606f216022bc691130
nahutrovato <trovatonahuel@gmail.com>
2022-05-18 13:07:50 -0300

    Drag and drop tests

--- src/main/java/DragAndDrop/DragAndDrop.java
+++ src/main/java/DragAndDrop/DragAndDrop.java
@@ -3,12 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 public class DragAndDrop {
 
     public static void main (String [] args) {
         System.setProperty("webdriver.chrome.driver","src\\main\\resources\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        String url = "https://demoqa.com/droppable";
 
 
 
