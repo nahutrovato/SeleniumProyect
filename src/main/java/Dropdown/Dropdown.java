55ee05a1adb54e09eedaa039e30224468048c21f
nahutrovato <trovatonahuel@gmail.com>
2022-05-18 12:25:40 -0300

    Dropdown test

---
+++ src/main/java/Dropdown/Dropdown.java
@@ -0,0 +1,19 @@
+package Dropdown;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+
+public class Dropdown {
+
+    public static void main (String [] args){
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://demoqa.com/select-menu";
+
+
+
+    }
+}
